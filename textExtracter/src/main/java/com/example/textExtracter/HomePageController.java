ef10a131c7979b5bc60874e3bd0cc8aa6cbe5018
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-03-24 23:32:25 +0530

    Create HomePageController.java

---
+++ textExtracter/src/main/java/com/example/textExtracter/HomePageController.java
@@ -0,0 +1,16 @@
+package com.example.textExtracter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePageController {
+	
+	@RequestMapping("/")
+	public String homePage() {
+		System.out.println("ASd");
+		return "index";
+	}
+
+}
