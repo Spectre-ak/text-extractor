a7f6be67d7ee2fdc9b68f587c7c8bdcce723d395
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-03-24 23:27:50 +0530

    Create TextExtracterApplicationTests.java

---
+++ textExtracter/src/test/java/com/example/textExtracter/TextExtracterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.textExtracter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextExtracterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
